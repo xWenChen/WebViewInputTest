48cfe6d17973c64e632489d51d7cf1e04ee9fd8c
wchenzhang <wchenzhang@tencent.com>
2022-08-19 19:35:13 +0800

    修复网页加载失败的问题

--- app/src/main/java/com/example/webviewinputtest/MainActivity.kt
+++ app/src/main/java/com/example/webviewinputtest/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.webviewinputtest
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -25,6 +28,28 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "trigger del event")
             // TODO 通知观察者软键盘删除键点击事件触发了
             false
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                val url = request.url.toString()
+                return try {
+                    if (url.startsWith("http:") || url.startsWith("https:")) {
+                        view.loadUrl(url)
+                        false
+                    } else {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                        startActivity(intent)
+                        true
+                    }
+                } catch (e: Exception) {
+                    Log.e(TAG, "", e)
+                    true
+                }
+            }
         }
 
         webView.settings.apply {
